119b3b0a14347c3ba2aca29941f4e62b78b0fe6e
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-07 00:15:30 +0330

    Add cin & cout

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
 int main() {
+    double a, b, result;
+    char op;
+
+    cout << "یک عدد وارد کنید: ";
+    cin >> a;
+
+    cout << "عملگر (+ - * /): ";
+    cin >> op;
+
+    cout << "عدد دوم را وارد کنید: ";
+    cin >> b;
+
+    switch (op) {
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': result = a / b; break;
+        default:
+            cout << "عملگر نامعتبر!" << endl;
+            return 0;
+    }
+
+    cout << "نتیجه: " << result << endl;
     return 0;
 }
